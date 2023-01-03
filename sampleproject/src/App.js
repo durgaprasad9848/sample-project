eef6f3346752a0f5c6115bf738e1a2d1ddd545c2
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2023-01-03 16:34:31 +0530

    sample project

---
+++ sampleproject/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+ 
+import UsersList from './Components/Users/UsersList.js';
+import AddUser from './Components/Users/AddUser.js';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
